587a6d62ef76292f8221c8e54a94e70540df11de
JackNarvaez <janarvaez@unal.edu.co>
2023-11-05 00:00:38 -0500

    Add bash script

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+files=('sedov.f90' 'main.f90')
+fflags='-O3'
+out='sedov'
+
+clear
+
+gfortran ${fflags} -o ${out} ${files[@]}
+./${out}
+
+gnuplot -p plot.gp
+
+rm -f sedov *.mod  
